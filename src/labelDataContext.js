d305f940390c55859e73d326cd55076b8cf02388
Dmitry Rudenko <dimarudenk@gmail.com>
2023-05-04 17:08:09 +0300

    111

--- src/labelDataContext.js
+++ src/labelDataContext.js
@@ -3,22 +3,54 @@ import React, { useState, createContext } from "react";
 export const LabelContext = createContext();
 
 export const LabelProvider = (props) => {
+  const [abylity, setAbylity] = useState([]);
+  const [personClass, setPersonClass] = useState(
+    {
+      title: "",
+      костьХитов: 0,
+      начальныеХиты: 0,
+      броня: [],
+      оружие: [],
+      инструменты: [],
+      спасброски: [],
+      навыки: {
+        count: 0,
+        items:[""],
+      },
+      классЗащиты: "10 + ловкость"
+    }
+  );
   const [page, setPage] = useState(0);
   const [labelInfo, setlabelInfo] = useState({
+    charRandom: {
+      str: "",
+      dex: "",
+      con: "",
+      int: "",
+      wis: "",
+      cha: ""
+    },
+    mod:{
+      str: 0,
+      dex: 0,
+      con: 0,
+      int: 0,
+      wis: 0,
+      cha: 0,
+    },
     sender: {
       name: "",
-      street: "",
-      city: "",
-      state: "",
-      zipCode: ""
+      str: 10,
+      dex: 10,
+      con: 10,
+      int: 10,
+      wis: 10,
+      cha: 10,
+      race: "",
+      raceDetails: "",
+      class: ""
     },
-    recevier: {
-      name: "",
-      street: "",
-      city: "",
-      state: "",
-      zipCode: ""
-    },
+     
     weight: "",
     shippingOption: "1"
   });
@@ -30,9 +62,24 @@ export const LabelProvider = (props) => {
   const prevPage = () => {
     setPage(page - 1);
   };
-  console.log(page, "page");
   const handleChange = (prop) => (event) => {
     setlabelInfo({ ...labelInfo, [prop]: event.target.value });
+  };
+
+  const setAbylityValue = (event) => {
+    if(event.target.checked){
+      setAbylity([...abylity, event.target.value]);
+    }else{
+      setAbylity(abylity.filter(item => item !== event.target.value));
+    }
+  };
+
+  const setSenderInfoValue = (prop, event) => {
+
+    setlabelInfo({
+      ...labelInfo,
+      sender: { ...labelInfo.sender, [prop]:event.target.innerHTML }
+    });
   };
 
   const setSenderInfo = (prop) => (event) => {
@@ -41,6 +88,36 @@ export const LabelProvider = (props) => {
       sender: { ...labelInfo.sender, [prop]: event.target.value }
     });
   };
+
+  const setModRaceValue = (value) => {
+    setlabelInfo({
+      ...labelInfo,
+      sender: {...labelInfo.sender, "race": value.title},
+      mod: { ...labelInfo.mod, "str": value.str,
+      "dex": value.dex,
+      "con": value.con,
+      "wis": value.wis,
+      "int": value.int,
+      "cha": value.cha, }
+    });
+  };
+
+  const setPersonClassValue = (value) => {
+    setPersonClass(value);
+  };
+
+  const setRandomInfoValue = () => {
+    setlabelInfo({
+      ...labelInfo,
+      charRandom: { ...labelInfo.charRandom, "str": Math.floor(Math.random() * (18 - 8 + 1)) + 8,
+      "dex": Math.floor(Math.random() * (18 - 8 + 1)) + 8,
+      "con": Math.floor(Math.random() * (18 - 8 + 1)) + 8,
+      "wis": Math.floor(Math.random() * (18 - 8 + 1)) + 8,
+      "int": Math.floor(Math.random() * (18 - 8 + 1)) + 8,
+      "cha": Math.floor(Math.random() * (18 - 8 + 1)) + 8, }
+    });
+  };
+
   const setRecevierInfo = (prop) => (event) => {
     setlabelInfo({
       ...labelInfo,
@@ -48,11 +125,11 @@ export const LabelProvider = (props) => {
     });
   };
   const steps = [
-    { title: "Get Sender addrtess" },
-    { title: "Get Reciver addrtess" },
-    { title: "Get Weight" },
-    { title: "Get Shipping Option" },
-    { title: "confirm" }
+    { title: "Дефолтные характеристики" },
+    { title: "Раса" },
+    { title: "Класс" },
+    { title: "Навыки" },
+    { title: "Финиш" }
   ];
 
   return (
@@ -60,12 +137,19 @@ export const LabelProvider = (props) => {
       value={{
         page,
         steps,
+        abylity,
+        personClass,
         nextPage,
         prevPage,
         labelInfo,
         handleChange,
         setSenderInfo,
-        setRecevierInfo
+        setSenderInfoValue,
+        setRandomInfoValue,
+        setRecevierInfo,
+        setModRaceValue,
+        setPersonClassValue,  
+        setAbylityValue     
       }}
     >
       {props.children}
